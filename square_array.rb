d5bdb8878719c180b1400f8c951518e60d700f6d
Marcus Moreno <legendary.1198@gmail.com>
2020-07-09 22:23:11 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,11 @@
 def square_array(array)
+ 
   new_array = []
+ 
   array.each do |numbers|
-     numbers ** 2
-    new_array << "#{numbers ** 2}"
+    
+    new_array <<  numbers ** 2
    
-    
-  
-    
   end
   new_array
 end
